d00f33d2daf4bef44296b5b56302d6f091f31c6b
Walker Peredo <walkerperedo@gmail.com>
2022-06-28 07:55:25 -0400

    bodega agregada

--- front/src/pages/Bodega/Bodega.jsx
+++ front/src/pages/Bodega/Bodega.jsx
@@ -1,5 +1,30 @@
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 export default function Bodega() {
-  return <div>Bodega</div>;
+  return (
+    <>
+      <Typography variant="h4">Bodega</Typography>
+      <Paper elevation={3}>
+        <Grid>
+          <Typography variant="h5">Ingredintes y Cantidades</Typography>
+          <List>
+            <ListItem>
+              <ListItemText primary="tomato 2" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="onion 1" />
+            </ListItem>
+          </List>
+        </Grid>
+      </Paper>
+    </>
+  );
 }
