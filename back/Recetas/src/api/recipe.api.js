0ff3fcba5595c7c50124d9a1515213cd483b181f
Walker Peredo <walkerperedo@gmail.com>
2022-06-29 15:38:26 -0400

    recipe api created

---
+++ back/Recetas/src/api/recipe.api.js
@@ -0,0 +1,26 @@
+const RecipeService = require("../services/recipe.service");
+const { STATUS_CODES } = require("../utils/app-errors");
+
+module.exports = (app) => {
+  const service = new RecipeService();
+
+  app.get("/recipes", async (req, res) => {
+    try {
+      const data = await service.GetAllRecipes();
+      return res.status(STATUS_CODES.OK).json(data);
+    } catch (error) {
+      res.status(STATUS_CODES.BAD_REQUEST).send(error.toString());
+      // next(error);
+    }
+  });
+
+  app.get("/recipe/:recipeId", async (req, res) => {
+    try {
+      const recipeId = req.params.recipeId;
+      const recipe = await service.GetRecipeById({ recipeId });
+      return res.status(STATUS_CODES.OK).json(recipe);
+    } catch (error) {
+      res.status(STATUS_CODES.BAD_REQUEST).send(error.toString());
+    }
+  });
+};
--- back/Recetas/src/express-app.js
+++ back/Recetas/src/express-app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { appEvents } = require("./api");
 const HandleErrors = require("./utils/error-handler");
+const recipeApi = require("./api/recipe.api");
 
 module.exports = async (app) => {
   app.use(express.json());
@@ -13,6 +14,7 @@ module.exports = async (app) => {
   appEvents(app);
 
   //api
+  recipeApi(app);
 
   //error handling
   app.use(HandleErrors);
